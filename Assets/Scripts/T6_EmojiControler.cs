b7ee9b5f50ac725fa25d289bee2948295e2b9f5a
Franci Algera <algera.franci@gmail.com>
2020-11-02 16:45:24 +0100

    Ligne + rando spawn

---
+++ Assets/Scripts/T6_EmojiControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T6_EmojiControler : MonoBehaviour
+{
+    private Transform emojiTransform = null;
+    public float speed = 5f;
+
+    private void Start()
+    {
+        emojiTransform = GetComponent<Transform>();
+        speed = Random.Range(2 , 3.5f) * Time.deltaTime;
+    }
+    private void Update()
+    {
+        emojiTransform.Translate(-speed, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //perdre un point de vie
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/T6_EmojiSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T6_EmojiSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject happyEmoji;
+    [SerializeField] private GameObject sadEmoji;
+    [SerializeField] private GameObject angryEmoji;
+    [SerializeField] private GameObject[] lines;
+
+    private float timer = 3;
+    private bool isSpawning = false;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            isSpawning = true;
+            SpawnEmoji();
+        }
+    }
+
+
+    private void SpawnEmoji()
+    {
+        int emoji = Random.Range(0, 3);
+        int line = Random.Range(0, 5);
+        switch (emoji)
+        {
+            case 0:
+                GameObject obj = Instantiate(happyEmoji, lines[line].transform);
+                break;
+            case 1:
+                obj = Instantiate(sadEmoji, lines[line].transform);
+                break;
+            case 2:
+                obj = Instantiate(angryEmoji, lines[line].transform);
+                break;
+        }
+        timer = 3;
+        isSpawning = false;
+    }
+}
