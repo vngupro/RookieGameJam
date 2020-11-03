16aaeb494811b8805fcd06d550cabd7b560c0e87
nguyen <vngu.pro@gmail.com>
2020-11-03 16:51:13 +0100

    player mov update

--- Assets/Scripts/T6_PlayerMovement.cs
+++ Assets/Scripts/T6_PlayerMovement.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 
 public class T6_PlayerMovement : MonoBehaviour
 {
-    int pos = 0;
-    void Start()
-    {
-        
-    }
+    
+    float inputVertical;
+    float timer = 0.1f;
+    [SerializeField] float timeInput = 0.1f;
+    [SerializeField] List<Transform> playerLineList = new List<Transform>();
+    private int currentLine = 2;
 
+    private void Start()
+    {
+        transform.position = playerLineList[currentLine].position;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && pos < 2)
+        inputVertical = Input.GetAxisRaw("Vertical");
+
+        //if (Input.GetKeyDown(KeyCode.Z) && pos < 2)
+        //{
+        //    gameObject.transform.position = new Vector2(transform.position.x, transform.position.y + 3);
+        //    pos++;
+        //}
+        //if (Input.GetKeyDown(KeyCode.S) && pos > -2)
+        //{
+        //    gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 3);
+        //    pos--;
+        //}
+
+        timer -= Time.deltaTime;
+        if (inputVertical == 1 && timer < 0 && currentLine > 0)
         {
-            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y + 3);
-            pos++;
-        }
-        if (Input.GetKeyDown(KeyCode.S) && pos > -2)
+            currentLine--;
+            transform.position = playerLineList[currentLine].position;
+            timer = timeInput;
+        }else if (inputVertical == -1 && timer < 0 && currentLine < playerLineList.Count - 1)
         {
-            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 3);
-            pos--;
+            currentLine++;
+            transform.position = playerLineList[currentLine].position;
+            timer = timeInput;
         }
     }
 }
