83ac48b9906ea991e03b77aa76c01dfcee8e4b57
nguyen <vngu.pro@gmail.com>
2020-11-04 17:49:41 +0100

    background infinite

---
+++ Assets/Scripts/T6_BackgroundSystem/T6_BackgroundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T6_BackgroundManager : MonoBehaviour
+{
+    [SerializeField] List<Transform> backgroundList = new List<Transform>();
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/T6_BackgroundSystem/T6_ParallaxEffect.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class T6_ParallaxEffect : MonoBehaviour
+{
+    [SerializeField] private float movingSpeed = 0.1f;
+    [SerializeField] private float id;
+    [SerializeField] Transform before;
+    [SerializeField] Transform after;
+    private float length, startPosX;
+    private Vector3 spriteSize;
+    private float distance = 0.0f;
+    private void Start()
+    {
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPosX = transform.position.x;
+    }
+    private void Update()
+    {
+        InfiniteBackground();
+    }
+
+    public void InfiniteBackground()
+    {
+        distance = transform.position.x - startPosX;
+        Debug.Log("Length = " + length);
+        Debug.Log("Distance = " + distance);
+        transform.position = new Vector3(transform.position.x - movingSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+
+        if (distance < -length * id && transform.position.x != startPosX)
+        {
+            transform.position = new Vector3(before.position.x + length - 0.1f, transform.position.y, transform.position.z);
+        }
+    }
+
+}
