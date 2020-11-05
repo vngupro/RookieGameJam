b69d7067a6275e8e67c4d53b2139d1f317515f34
Noé <noeguillaumin@gmail.com>
2020-11-05 14:54:46 +0100

    ajout tutorial prefab
    prefab --> tutorial Creer un gameobject en 0 0 0 avec tous les éléments du jeu dedans
    Le script est dans le TUTORIAL gameobject

---
+++ Assets/Scripts/T6_PlayGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T6_PlayGame : MonoBehaviour
+{
+    public GameObject game;
+    public GameObject tuto;
+    public void PlayGame()
+    {
+        tuto.SetActive(false);
+        game.SetActive(true);
+    }
+}
