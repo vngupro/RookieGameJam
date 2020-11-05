4b3f1f42c21690d2c882497b83f61e2de5b75897
nguyen <vngu.pro@gmail.com>
2020-11-05 21:37:02 +0100

    sound begin

---
+++ Assets/Scripts/T6_SoundManager/T6_Sound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class T6_Sound
+{
+    public AudioClip clip;
+
+    [Range(0.0f, 1.0f)]
+    public float volume;
+    [Range(0.1f, 0.3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+}
---
+++ Assets/Scripts/T6_SoundManager/T6_SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class T6_SoundManager : MonoBehaviour
+{
+    public T6_Sound[] soundList;
+
+    private void Awake()
+    {
+        foreach(T6_Sound sound in soundList)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
